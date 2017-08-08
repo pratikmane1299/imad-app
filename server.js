a16c94e0c6372e296658eaa171fa45d22b1f448a
pratikmane1299 <30666584+pratikmane1299@users.noreply.github.com>
2017-08-08 12:32:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui Firsturl', function (req, res) {
+app.get('/ui/Firsturl', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'Firsturl.html'));
 });
 
